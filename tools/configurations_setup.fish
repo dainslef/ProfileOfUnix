fd63cd71da71d22a8db8f72507ad093c779cdf11
dainslef <dainslef@outlook.com>
2022-05-24 02:52:02 +0800

    Add ansible and dunst config setup logic to configuration setup tool.

--- tools/configurations_setup.fish
+++ tools/configurations_setup.fish
@@ -15,13 +15,13 @@ ln -sf $REPO_PATH/.gitignore ~/.gitignore
 echo OK
 
 # Fish
-echo -ne "Set up Fish shell ... \r"
+echo -ne "Set up Fish shell ... "
 if ! [ -e ~/.config/omf ] # Check if OMF has areadly downloaded.
     curl -L https://get.oh-my.fish | fish
 end
 omf install bobthefish
 ln -sf $REPO_PATH/shell/config.fish ~/.config/fish/config.fish
-echo "Set up Fish shell ... OK"
+echo OK
 
 # VIM
 echo -n "Set up VIM ... "
@@ -31,11 +31,26 @@ end
 ln -sf $REPO_PATH/editor/.vimrc ~/.vimrc
 echo OK
 
+# Ansible
+echo -n "Set up Ansible ... "
+if ! [ -e ~/.ansible/hosts ]
+    cp $REPO_PATH/ansible/hosts ~/.ansible/hosts
+end
+ln -sf $REPO_PATH/ansible/ansible.cfg ~/.ansible.cfg
+echo OK
+
 # Window Manager
 if type -q qtile
+    # Link the Qtile configuration.
     echo -n "Set up Qtile ... "
     mkdir -p ~/.config/qtile
     ln -sf $REPO_PATH/window_manager/qtile/config.py ~/.config/qtile/config.py
+    echo OK
+
+    # Link the Dunst configuration.
+    echo -n "Set up Dunst ... "
+    mkdir -p ~/.config/dunst
+    ln -sf $REPO_PATH/dunstrc ~/.config/dunst/dunstrc
     echo OK
 end
 if type -q awesome
@@ -45,12 +60,13 @@ if type -q awesome
     echo OK
 end
 
-# Set up theme manually only if use Window Manager
+# Link the theme configuration manually only if use Window Manager
 if type -q qtile || type -q awesome
     # GTK theme
     echo -ne "Set up GTK theme ... "
     mkdir -p ~/.config/gtk-3.0
     ln -sf $REPO_PATH/theme/settings.ini ~/.config/gtk-3.0/settings.ini
+    ln -sf $REPO_PATH/theme/.gtkrc-2.0 ~/.gtkrc-2.0
     echo OK
 
     # Cursor theme

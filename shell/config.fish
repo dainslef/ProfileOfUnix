bf6405f89179701cdbced9766977b552c13ee215
dainslef <dainslef@outlook.com>
2024-03-19 15:41:50 +0800

    Update configurations for fish shell and Kitty terminal.
    1. Rewrite and remove some unncessary contents in fish configuration.
    2. Set the title bar process name display option in omf bobthefish theme.
    3. Use VSCode as default editor when possible.
    4. Add a Kitty terminal configuration for macOS.

--- shell/config.fish
+++ shell/config.fish
@@ -4,179 +4,164 @@
 # $ curl -L https://get.oh-my.fish | fish
 # $ omf install bobthefish
 
-# Fish shell and Oh-My-Fish file directory:
+# Fish shell and Oh-My-Fish file and directory:
+# ~/.config/fish/conf.d/omf.fish
 # ~/.local/share/omf
-# ~/.local/share/fish
 # ~/.cache/omf
-# ~/.config/fish
 
 
 
-# --- Define custom functions---
-
-# Set the default user.
-function set_default_user
-    # Use "set" to create a variable, use "-g" to create a global variable.
-    set -g default_user $argv[1]
-end
-
-# Check user, set the custom environment variables
+# Set the custom environment variables.
 function env_config
 
+    # Set language environment.
+    # Use "set -x" to create a environment variable.
+    # Use "-xg" to set this environment variable as a global environment variable.
+    set -xg LANG en_US.UTF-8
+    set -xg LC_ALL en_US.UTF-8
+
+    # Append local path.
     set PATH $PATH /usr/local/bin /usr/local/sbin
 
-    if [ (whoami) = "$default_user" ]
+    # Don't set envrionment for root.
+    if [ (whoami) = root ]
+        return
+    end
 
-        # Check OS type and set the different environment variables
-        if [ (uname) = Darwin ] # Darwin kernel means in macOS
+    # Check OS type and set the different environment variables
+    if [ (uname) = Darwin ] # Darwin kernel means in macOS
 
-            # Set Python PIP binary path.
-            set python_version (python3 -V | awk -F' ' '{ print $2 }' | awk -F'.' '{ print $1 "." $2 }')
-            set pip_bin ~/Library/Python/$python_version/bin
+        # Add Homebrew path for macOS ARM plantform.
+        set PATH $PATH /opt/homebrew/bin
 
-            # From macOS 12 (Monterey), command 'ls' don't print with color,
-            # need to set the 'CLICOLOR' for color output.
-            set -gx CLICOLOR 1 # Use ANSI color sequences to distinguish file types
+        # Set the environment variable for Homebrew Bottles mirror (no longer need when use clash tun)
+        # set -xg HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/bottles
+        # set -xg HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
 
-            # Set the environment variable for Homebrew Bottles mirror (no longer need when use clash tun)
-            # set -xg HOMEBREW_BOTTLE_DOMAIN https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/bottles
-            # set -xg HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+    else if [ (uname) = Linux ]
 
-        else if [ (uname) = Linux ]
+        # Remember "alias" is synatx candy for "function" in fish shell,
+        # alias command can not run at background like "xxx &".
+        # function idea
+        #     ~/Public/idea-IU/bin/idea.sh $argv &
+        # end
 
-            # Set Python Pip binary path.
-            set pip_bin ~/Public/Python/bin
-
-            # Remember "alias" is synatx candy for "function" in fish shell,
-            # alias command can not run at background like "xxx &".
-            # function idea
-            #     ~/Public/idea-IU/bin/idea.sh $argv &
-            # end
-
-            # Select input method by desktop environment.
-            # Gnome XDG_SESSION_DESKTOP will be gnome-xorg or gnome-wayland
-            # By default, string match will return match content to standard output,
-            # use output redirection to ignore this output.
-            string match 'gnome*' "$XDG_SESSION_DESKTOP" >/dev/null # match return 0 (normal)
-            if [ $status = 0 ] || [ "$DESKTOP_SESSION" = pantheon ]
-                set input_method ibus
-            else
-                set input_method fcitx
-            end
-
-            # Set the input environment variables.
-            set -gx GTK_IM_MODULE $input_method
-            set -gx QT_IM_MODULE $input_method
-            set -gx XMODIFIERS "@im=$input_method"
-            set -gx CLASSPATH $CLASSPATH:.
-
-            # Disable Qt auto scaling, to fix VLC UI scaling problem.
-            set -gx QT_AUTO_SCREEN_SCALE_FACTOR 0
-
-            # Set Qt style.
-            if [ "$XDG_SESSION_DESKTOP" = xfce ]
-                # Need to set both environment variables,
-                # only set QT_STYLE_OVERRIDE=gtk2 will cause application crash.
-                set -gx QT_STYLE_OVERRIDE gtk2
-                set -gx QT_QPA_PLATFORMTHEME gtk2
-            end
-
-        end
-
-        # Set language environment.
-        # Use "set -x" to create a environment variable.
-        # Use "-xg" to set this environment variable as a global environment variable.
-        set -xg LANG en_US.UTF-8
-        set -xg LC_ALL en_US.UTF-8
-
-        # Add the local binary path.
-        if [ -e ~/.local/bin ]
-            set PATH $PATH ~/.local/bin
-        end
-
-        # Add Python pip package binary path.
-        if [ -e "$pip_bin" ]
-            set PATH $PATH $pip_bin
-        end
-
-        # Add Haskell GHCup binary path.
-        if [ -e ~/.ghcup/bin ]
-            set PATH $PATH ~/.ghcup/bin
-        end
-
-        # Add DotNet Core tools path.
-        if [ -e ~/.dotnet/tools ]
-            set PATH $PATH ~/.dotnet/tools
-        end
-
-        # Set the environment variable for rustup mirror (no longer need when use clash tun).
-        # Install rust stable toolchain: $ rustup toolchain install stable.
-        set -xg RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
-
-        # Add Rust Cargo binary path.
-        if [ -e ~/.cargo/bin ]
-            set PATH $PATH ~/.cargo/bin
-        end
-
-        # Preferred editor for local and remote sessions.
-        if [ -n "$SSH_CONNECTION" ]
-            set -xg EDITOR nano
+        # Select input method by desktop environment.
+        # Gnome XDG_SESSION_DESKTOP will be gnome-xorg or gnome-wayland
+        # By default, string match will return match content to standard output,
+        # use output redirection to ignore this output.
+        string match 'gnome*' "$XDG_SESSION_DESKTOP" >/dev/null # match return 0 (normal)
+        if [ $status = 0 -o "$DESKTOP_SESSION" = pantheon ]
+            set input_method ibus
         else
-            set -xg EDITOR vim
+            set input_method fcitx
         end
 
-        # Set terminfo when using Kitty terminal.
-        if [ "$TERM" = xterm-kitty ]
-            set -gx TERM xterm-256color
-        end
+        # Set the input environment variables.
+        set -gx GTK_IM_MODULE $input_method
+        set -gx QT_IM_MODULE $input_method
+        set -gx XMODIFIERS "@im=$input_method"
+        set -gx CLASSPATH $CLASSPATH:.
 
-        # For VSCode shell integration support.
-        string match -q "$TERM_PROGRAM" vscode and . (code --locate-shell-integration-path fish)
+        # Disable Qt auto scaling, to fix VLC UI scaling problem.
+        set -gx QT_AUTO_SCREEN_SCALE_FACTOR 0
+
+        # Set Qt style.
+        if [ "$XDG_SESSION_DESKTOP" = xfce ]
+            # Need to set both environment variables,
+            # only set QT_STYLE_OVERRIDE=gtk2 will cause application crash.
+            set -gx QT_STYLE_OVERRIDE gtk2
+            set -gx QT_QPA_PLATFORMTHEME gtk2
+        end
 
     end
+
+    # Add the local binary path.
+    if [ -e ~/.local/bin ]
+        set PATH $PATH ~/.local/bin
+    end
+
+    # Add Python pip package binary path (Use Python venv).
+    if [ -e ~/Public/Python/bin ]
+        set PATH $PATH $pip_bin
+    end
+
+    # Add Haskell GHCup binary path.
+    if [ -e ~/.ghcup/bin ]
+        set PATH $PATH ~/.ghcup/bin
+    end
+
+    # Add DotNet Core tools path.
+    if [ -e ~/.dotnet/tools ]
+        set PATH $PATH ~/.dotnet/tools
+    end
+
+    # Set the environment variable for rustup mirror (no longer need when use clash tun).
+    # Install rust stable toolchain: $ rustup toolchain install stable.
+    set -xg RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+
+    # Add Rust Cargo binary path.
+    if [ -e ~/.cargo/bin ]
+        set PATH $PATH ~/.cargo/bin
+    end
+
+    # Set preferred editor.
+    if [ -n "$DISPLAY" -o (uname) = Darwin ]
+        and type -q code
+        set -xg EDITOR "code --wait"
+    else if type -q vim
+        set -xg EDITOR vim
+    else
+        set -xg EDITOR vi
+    end
+
+    # For VSCode shell integration support.
+    string match -q "$TERM_PROGRAM" vscode and . (code --locate-shell-integration-path fish)
 
 end
 
 # Set the theme configuration.
 function theme_config
-
     # Set the theme, only in Linux GUI and macOS.
     if [ -n "$DISPLAY" -o (uname) = Darwin ]
         omf theme bobthefish
-        # Set theme color for bobthefish, more theme configs see https://github.com/oh-my-fish/theme-bobthefish.
+        # Theme bobthefish configs, more theme configs see https://github.com/oh-my-fish/theme-bobthefish.
         # Override the default greeting at ~/.config/fish/functions/fish_greeting.fish or refine function.
-        set -g theme_color_scheme nord # light
+        set -g theme_color_scheme nord # Set theme color for bobthefish.
+        set -g theme_title_display_process yes # Set progress name in title bar.
         set -g theme_date_format "+%b/%d/%Y [%a] %R:%S"
     else # Use default in Non-GUI environment.
         omf theme default
     end
-
 end
 
-# Call function if oh-my-fish is installed.
+# Call function if oh-my-posh is installed.
 if [ -n "$OMF_PATH" ]
-    set_default_user dainslef
+    and status is-interactive
+    # Set environment config.
     env_config
+    # Set the theme.
     theme_config
 end
+
+# Delete defined functions and variables.
+# Use "-e" means to erase a function/variable.
+functions -e env_config theme_config
 
 
 
 # --- Override functions  ---
-
 # In fish shell, function which named with "fish_greeting" will override default greeting.
 function fish_greeting
 
-    if [ (whoami) = "$default_user" ]
-        if [ (uname) = Darwin ]
-            set show_os_version (uname -srnm)
-        else if [ -n "$DISPLAY" ]
-            set show_os_version (uname -ornm)
-        end
+    if [ (uname) = Darwin ]
+        set show_os_version (uname -srnm)
+    else if [ -n "$DISPLAY" ]
+        set show_os_version (uname -ornm)
     end
 
     # Print welcome message in macOS or Linux GUI.
-    if [ -n "$show_os_version" ]
+    if [ -n "$show_os_version" -a (whoami) != root ]
         set_color $fish_color_autosuggestion # Set greet color.
         echo (uptime)
         echo " $show_os_version"
@@ -202,13 +187,3 @@ function fish_greeting
     end
 
 end
-
-
-
-# --- Clean the environment  ---
-
-# Delete defined functions and variables.
-# Use "-e" means to erase a function/variable.
-functions -e set_default_user
-functions -e env_config
-functions -e theme_config
